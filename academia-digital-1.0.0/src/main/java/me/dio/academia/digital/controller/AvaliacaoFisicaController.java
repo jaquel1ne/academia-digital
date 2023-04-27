5b56b28cd29f0aa0066b455959708ca9bff7b4bb
jaquel1ne <jaquilinas@gmail.com>
2023-04-27 09:59:45 -0300

    first commit

---
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
@@ -0,0 +1,31 @@
+package me.dio.academia.digital.controller;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import me.dio.academia.digital.repository.AvaliacaoFisicaRepository;
+import me.dio.academia.digital.service.impl.AvaliacaoFisicaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/avaliacoes")
+public class AvaliacaoFisicaController {
+
+    @Autowired
+    private AvaliacaoFisicaServiceImpl service;
+
+    @PostMapping
+    public AvaliacaoFisica create(@RequestBody AvaliacaoFisicaForm form) {
+        return service.create(form);
+    }
+
+
+
+}
